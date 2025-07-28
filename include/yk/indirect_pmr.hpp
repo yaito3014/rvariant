5adaf3fb8b8c53f208c3482f2f8e8d91f62f7de4
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2025-07-29 03:55:41 +0900

    Add `pmr`

---
+++ include/yk/indirect_pmr.hpp
@@ -0,0 +1,16 @@
+﻿#ifndef YK_INDIRECT_PMR_HPP
+#define YK_INDIRECT_PMR_HPP
+
+#include <yk/indirect.hpp>
+
+#include <memory_resource>
+
+
+namespace yk::pmr {
+
+template<class T>
+using indirect = yk::indirect<T, std::pmr::polymorphic_allocator<T>>;
+
+} // yk::pmr
+
+#endif
---
+++ include/yk/rvariant/recursive_wrapper_pmr.hpp
@@ -0,0 +1,15 @@
+﻿#ifndef YK_RVARIANT_RECURSIVE_WRAPPER_PMR_HPP
+#define YK_RVARIANT_RECURSIVE_WRAPPER_PMR_HPP
+
+#include <yk/rvariant/recursive_wrapper.hpp>
+
+#include <memory_resource>
+
+namespace yk::pmr {
+
+template<class T>
+using recursive_wrapper = yk::recursive_wrapper<T, std::pmr::polymorphic_allocator<T>>;
+
+} // yk::pmr
+
+#endif
