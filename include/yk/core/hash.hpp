33603369137cf34bcf38ce85c70bfe4550405cf2
yaito3014 <ykakeyama3014@gmail.com>
2025-08-06 07:50:19 +0900

    Add copyright header

--- include/yk/core/hash.hpp
+++ include/yk/core/hash.hpp
@@ -1,6 +1,11 @@
 #ifndef YK_CORE_HASH_HPP
 #define YK_CORE_HASH_HPP
 
+// Copyright 2025 Yaito Kakeyama
+// Copyright 2025 Nana Sakisaka
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
 #include <yk/core/type_traits.hpp>
 
 #include <type_traits>
